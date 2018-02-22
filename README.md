54fcaa10ac8936c9394ae3627d79a96c4887ae2f
youmeek <jn3.141592654@gmail.com>
2018-02-22 23:16:10 +0800

    1.完善细节 2.补充立即执行方法

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # SpringBootQuartz
-Quartz with Spring Boot
 
-Visit <http://blog.csdn.net/u012907049/article/details/73801122> to see more details
+- Quartz with Spring Boot
+- 相关文章介绍：<http://blog.csdn.net/u012907049/article/details/73801122>
+- 启动项目后访问：<http://127.0.0.1:8080/JobManager.html>
+- 添加的时候：
+	- 任务名称填写 Job 类完整包路径 + 类名，比如：com.example.demo.job.NewJob
+	- 任务分组名称自己任意填写字符串。其中：**分组名+任务名称组成一个完整的唯一任务**。如果两者一样已经存在一样的，则无法添加成功，会抛异常：because one already exists with this identification
+	- 表达式填写Cron表达式，比如：`0/3 * * * * ?`，在线表达：<http://cron.qqe2.com/>
--- demo/src/main/java/com/example/demo/job/HelloJob.java
+++ demo/src/main/java/com/example/demo/job/HelloJob.java
@@ -1,11 +1,21 @@
 package com.example.demo.job;
 
-import java.util.Date;  
-import org.slf4j.Logger;  
-import org.slf4j.LoggerFactory;   
-import org.quartz.JobExecutionContext;  
-import org.quartz.JobExecutionException;  
-  
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/**
+ * @DisallowConcurrentExecution 注解的作用：
+ * 此标记用在实现Job的类上面,意思是不允许并发执行,按照我之前的理解是 不允许调度框架在同一时刻调用Job类，后来经过测试发现并不是这样，而是Job(任务)的执行时间[比如需要10秒]大于任务的时间间隔[Interval（5秒)],
+ * 那么默认情况下,调度框架为了能让 任务按照我们预定的时间间隔执行,会马上启用新的线程执行任务。否则的话会等待任务执行完毕以后 再重新执行！（这样会导致任务的执行不是按照我们预先定义的时间间隔执行）
+ * 设置 @DisallowConcurrentExecution 以后程序会等任务执行完毕以后再去执行，这样就不会再启用新的线程执行
+ * 资料：http://www.cnblogs.com/daxin/archive/2013/05/27/3101972.html
+ */
+@DisallowConcurrentExecution
 public class HelloJob implements BaseJob {  
   
     private static Logger _log = LoggerFactory.getLogger(HelloJob.class);  
@@ -13,10 +23,11 @@ public class HelloJob implements BaseJob {
     public HelloJob() {  
           
     }  
-     
+    
+    @Override
     public void execute(JobExecutionContext context)  
         throws JobExecutionException {  
-        _log.error("Hello Job执行时间: " + new Date());  
+        _log.info("Hello Job执行时间: " + new Date());  
           
     }  
 }  
